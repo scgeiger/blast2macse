f9604f44ea921f5ea032913ec457b45b5911e0a9
scgeiger <sarahcatgeiger@gmail.com>
2020-11-30 04:47:58 +0000

    Began work on plotting summary script, major updates in info for make-summary-table.pl

--- make-summary-table.pl
+++ make-summary-table.pl
@@ -12,7 +12,9 @@ my $ref_length;
 my $aln_length;
 my $dataset;
 my ($ntTS, $ntTV, $ntIN, $ntDEL, $ntAM, $ntFS, $aaMS, $aaNS, $aaSYN, $aaIN, $aaDEL, $aaFS, $aaAM); 
+my ($ntTS_R, $ntTV_R, $ntIN_R, $ntDEL_R, $ntAM_R, $ntFS_R, $aaMS_R, $aaNS_R, $aaSYN_R, $aaIN_R, $aaDEL_R, $aaFS_R, $aaAM_R);
 my ($ntHap, $ntSingHap, $nSegSite, $nSingSNPs, $n_ntSingHap, $n_ntMajorHap);
+my ($ntHap_R, $ntSingHap_R, $nSegSite_R, $nSingSNPs_R, $n_ntSingHap_R, $n_ntMajorHap_R);
 my ($PG_TajimaD, $PG_RozasR2, $PG_FuliF, $PG_FuliD, $PG_FuFs, $PG_pi, $PG_SegSite, $PG_Haplo, $PG_SingHaps, $PG_nSeqs); 
 my ($PG_TajimaD_R, $PG_RozasR2_R, $PG_FuliF_R, $PG_FuliD_R, $PG_FuFs_R, $PG_pi_R, $PG_SegSite_R, $PG_Haplo_R, $PG_SingHaps_R, $PG_nSeqs_R);
 my ($seq, $seqID, $row, $nRem);
@@ -88,12 +90,31 @@ $header      =  "dataset"     . "\t" .
                 "aaDEL"       . "\t" .
                 "aaFS"        . "\t" .
                 "aaAM"        . "\t" .
+                "ntTS_R"      . "\t" .
+                "ntTV_R"      . "\t" .
+                "ntIN_R"      . "\t" .
+                "ntDEL_R"     . "\t" .
+                "ntFS_R"      . "\t" .
+                "ntAM_R"      . "\t" .
+                "aaMS_R"      . "\t" .
+                "aaNS_R"      . "\t" .
+                "aaSYN_R"     . "\t" .
+                "aaIN_R"      . "\t" .
+                "aaDEL_R"     . "\t" .
+                "aaFS_R"      . "\t" .
+                "aaAM_R"      . "\t" . 
                 "ntHap"       . "\t" .
                 "ntSingHap"   . "\t" .
                 "nSegSite"    . "\t" .
-                "nSingSNPs"   . "\t" .
+#                "nSingSNPs"   . "\t" .
                 "n_ntSingHap" . "\t" .
                 "n_ntMajorHap". "\t" .
+                "ntHap_R"     . "\t" .
+                "ntSingHap_R" . "\t" .
+                "nSegSite_R"  . "\t" .
+#                "nSingSNPs_R" . "\t" .
+                "n_ntSingHap_R". "\t" .
+                "n_ntMajorHap_R". "\t" . 
                 "PG_TajimaD"  . "\t" .
                 "PG_RozasR2"  . "\t" .
                 "PG_FuliF"    . "\t" .
@@ -138,52 +159,71 @@ foreach $GENE (@all_genes) {
     open O, '>', $outfile or die "problem saving output to file\n";
         print O "#Analysis run on $TIMESTAMP, using gene $GENE.\n";
         print "$GENE: Didn't find any BLAST hits. Exiting table with defaults\n";
-        $output =  $dataset  . "\t" .
-                $GENE         . "\t" .
-                $db_nseqs     . "\t" .
-                "0"    . "\t" . #hit_nseqs
-                "NA"   . "\t" . #ref_length
-                $aln_length   . "\t" . #aln_length
-                "0"         . "\t" . #ntTS
-                "0"         . "\t" . #ntTV
-                 "0"   . "\t" . #ntIN
-                  "0"  . "\t" . #ntDEL
-                 "0"   . "\t" . #ntFS
-                 "0"   . "\t" . #ntAM
-                 "0"   . "\t" . #aaMS
-                 "0"   . "\t" . #aaNS
-                 "0"   . "\t" . #aaSYN
-                 "0"   . "\t" . #aaIN
-                 "0"   . "\t" . #aaDEL
-                 "0"   . "\t" . #aaDEL
-                 "0"   . "\t" .
-                 "0"    . "\t" .
-                "NA"   . "\t" .
-                "NA"   . "\t" .
-                "NA"  . "\t" .
-                "NA"  . "\t" .
-                "NA" . "\t" .
-                "NA"   . "\t" .
-                "NA"   . "\t" .
-                 "NA"   . "\t" .
-                 "NA"   . "\t" .
-                 "NA"   . "\t" .
-                 "NA"       . "\t" .
-                "NA"  . "\t" .
-                 "NA". "\t" .
-                "NA" . "\t" .
-                 "NA"   . "\t" .
-                 "NA"       . "\t" .
-                "NA" . "\t" .
-                "NA" . "\t" .
-                "NA"  . "\t" .
-                "NA"  . "\t" .
-                 "NA"  . "\t" .
-                 "NA"  . "\t" .
-                "NA".  "\t" .
-                "NA"  . "\t" .
-                "NA" . "\t" .
-                "NA"  . "\n";
+        $output =   $dataset    . "\t" .    # dataset
+                    $GENE       . "\t" .    # gene
+                    $db_nseqs   . "\t" .    # db_nseqs
+                    "0"         . "\t" .    # hit_nseqs
+                    "NA"        . "\t" .    # ref_length
+                    $aln_length . "\t" .    # aln_length
+                    "0"         . "\t" .    # ntTS
+                    "0"         . "\t" .    # ntTV
+                    "0"         . "\t" .    # ntIN
+                    "0"         . "\t" .    # ntDEL
+                    "0"         . "\t" .    # ntFS
+                    "0"         . "\t" .    # ntAM
+                    "0"         . "\t" .    # aaMS
+                    "0"         . "\t" .    # aaNS
+                    "0"         . "\t" .    # aaSYN
+                    "0"         . "\t" .    # aaIN
+                    "0"         . "\t" .    # aaDEL
+                    "0"         . "\t" .    # aaFS
+                    "0"         . "\t" .    # aaAM
+                    "0"         . "\t" .    # ntTS_R
+                    "0"         . "\t" .    # ntTV_R
+                    "0"         . "\t" .    # ntIN_R
+                    "0"         . "\t" .    # ntDEL_R
+                    "0"         . "\t" .    # ntFS_R
+                    "0"         . "\t" .    # ntAM_R
+                    "0"         . "\t" .    # aaMS_R
+                    "0"         . "\t" .    # aaNS_R
+                    "0"         . "\t" .    # aaSYN_R
+                    "0"         . "\t" .    # aaIN_R
+                    "0"         . "\t" .    # aaDEL_R
+                    "0"         . "\t" .    # aaFS_R
+                    "0"         . "\t" .    # aaAM_R
+                    "0"         . "\t" .    # ntHap
+                    "NA"        . "\t" .    # ntSingHap 
+                    "NA"        . "\t" .    # nSegSite
+#                    "NA"        . "\t" .    # nSingSNPs
+                    "NA"        . "\t" .    # n_ntSingHap
+                    "NA"        . "\t" .    # n_ntMajorHap
+                    "0"         . "\t" .    # ntHap_R
+                    "NA"        . "\t" .    # ntSingHap_R
+                    "NA"        . "\t" .    # nSegSite_R
+#                    "NA"        . "\t" .    # nSingSNPs_R
+                    "NA"        . "\t" .    # n_ntSingHap_R
+                    "NA"        . "\t" .    # n_ntMajorHap_R
+                    "NA"        . "\t" .    # PG_TajimaD
+                    "NA"        . "\t" .    # PG_RozasR2
+                    "NA"        . "\t" .    # PG_FuliF
+                    "NA"        . "\t" .    # PG_FuliD
+                    "NA"        . "\t" .    # PG_FuFs
+                    "NA"        . "\t" .    # PG_pi
+                    "NA"        . "\t" .    # PG_SegSite
+                    "NA"        . "\t" .    # PG_Haplo
+                    "NA"        . "\t" .    # PG_SingHaps
+                    "NA"        . "\t" .    # PG_nSeqs
+                    "NA"        . "\t" .    # nRem
+                    "NA"        . "\t" .    # PG_TajimaD_R
+                    "NA"        . "\t" .    # PG_RozasR2_R
+                    "NA"        . "\t" .    # PG_FuliF_R
+                    "NA"        . "\t" .    # PG_FuliD_R
+                    "NA"        . "\t" .    # PG_FuFs_R
+                    "NA"        . "\t" .    # PG_pi_R
+                    "NA"        . "\t" .    # PG_SegSite_R
+                    "NA"        . "\t" .    # PG_Haplo_R
+                    "NA"        . "\t" .    # PG_SingHaps_R
+                    "NA"        . "\n" ;    # PG_nSeqs_R
 
             print O $output;
             print S $output;
@@ -387,6 +427,118 @@ foreach $GENE (@all_genes) {
                 $nSegSite = scalar(keys(%hash));
             close F;
 
+            ##############################
+            # get codon mut data REMOVED #
+            ##############################
+            @temp = ();
+            %hash = ();
+            open F, '<', "$PARENTPATH/$GENE/$DATA_ID-removed.macse-codon-dist.tsv";
+                while ($row = <F>) {
+                    chomp $row;
+                    if ($row =~ /^#Date/) { next; } ;
+                    if ($row =~ /^#NT/) {
+                        $row =~ s/^[^||]*\|\|\s//;
+                        @temp = split /\,/, $row;
+                        foreach $temp (@temp) {
+                            $temp =~ s/^\s+//; #remove leading spaces
+                            if ($temp =~ /^Ambiguous/) {
+                                $temp =~ s/^[^:]*\://;
+                                $temp =~ s/^\s+//;
+                                $ntAM_R = $temp;
+                                #print "ntAM is $ntAM\n";
+                            }
+                            if ($temp =~ /^Transition/) {
+                                $temp =~ s/^[^:]*\://;
+                                $temp =~ s/^\s+//;
+                                $ntTS_R = $temp;
+                                #print "ntTS is $ntTS\n";
+                            }
+                            if ($temp =~ /^Transversion/) {
+                                $temp =~ s/^[^:]*\://;
+                                $temp =~ s/^\s+//;
+                                $ntTV_R = $temp;
+                                #print "ntTV is $ntTV\n";
+                            }
+                            if ($temp =~ /^Frameshift/) {
+                                $temp =~ s/^[^:]*\://;
+                                $temp =~ s/^\s+//;
+                                $ntFS_R = $temp;
+                                #print "ntFS is $ntFS\n";
+                            }
+                            if ($temp =~ /^Insertion/) {
+                                $temp =~ s/^[^:]*\://;
+                                $temp =~ s/^\s+//;
+                                $ntIN_R = $temp;
+                                #print "ntIN is $ntIN\n";
+                            }
+                            if ($temp =~ /^Deletion/) {
+                                $temp =~ s/^[^:]*\://;
+                                $temp =~ s/^\s+//; #remove leading spaces
+                                $ntDEL_R = $temp;
+                                #print "ntDEL is $ntDEL\n";
+                            }
+                        }
+                    }
+                    elsif ($row =~ /^#AA/) {
+                        $row =~ s/^[^||]*\|\|\s//;
+                        @temp = split /\,/, $row;
+                        foreach $temp (@temp) {
+                            $temp =~ s/^\s+//; #remove leading spaces
+                            if ($temp =~ /^Ambiguous/) {
+                                $temp =~ s/^[^:]*\://;
+                                $temp =~ s/^\s+//;
+                                $aaAM_R = $temp;
+                                #print "aaAM is $aaAM\n";
+                            }
+                            if ($temp =~ /^Missense/) {
+                                $temp =~ s/^[^:]*\://;
+                                $temp =~ s/^\s+//;
+                                $aaMS_R = $temp;
+                                #print "aaMS is $aaMS\n";
+                            }
+                            if ($temp =~ /^Nonsense/) {
+                                $temp =~ s/^[^:]*\://;
+                                $temp =~ s/^\s+//;
+                                $aaNS_R = $temp;
+                                #print "aaNS is $aaNS\n";
+                            }
+                            if ($temp =~ /^Synonymous/) {
+                                $temp =~ s/^[^:]*\://;
+                                $temp =~ s/^\s+//;
+                                $aaSYN_R = $temp;
+                                #print "aaSYN is $aaSYN\n";
+                            }
+                            if ($temp =~ /^Frameshift/) {
+                                $temp =~ s/^[^:]*\://;
+                                $temp =~ s/^\s+//;
+                                $aaFS_R = $temp;
+                                #print "aaFS is $aaFS\n";
+                            }
+                            if ($temp =~ /^Insertion/) {
+                                $temp =~ s/^[^:]*\://;
+                                $temp =~ s/^\s+//;
+                                $aaIN_R = $temp;
+                                #print "aaIN is $aaIN\n";
+                            }
+                            if ($temp =~ /^Deletion/) {
+                                $temp =~ s/^[^:]*\://;
+                                $temp =~ s/^\s+//; #remove leading spaces
+                                $aaDEL_R = $temp;
+                                #print "aaDEL is $aaDEL\n";
+                            }
+                        }
+                    }
+                    else {
+                        @temp = split /\t/, $row;
+                        if ($temp[3] eq "Transversion" || $temp[3] eq "Transition") {
+                            $pos = $temp[0];
+                            $hash{$pos} = 1;
+                        }
+                    }
+                }
+                $nSegSite_R = scalar(keys(%hash));
+            close F;    
+
             ####################### 
             # get uniqallele data #
             #######################
@@ -407,9 +559,7 @@ foreach $GENE (@all_genes) {
             #$ntHap [last position of column 0] #index 0 
             @temp = split /\t/, $uniq[$#uniq];
             $ntHap = $temp[0];
-            #print "ntHap is $ntHap\n";
             %hash = (); 
-            #n_ntMajorHap
             #[in final row, column 18 (last column), how many space delmitied items exist?]
             $n_ntMajorHap = $temp[$#temp] =~ tr{ }{ };
             #print "n_ntMajorHap is $n_ntMajorHap\n";
@@ -425,7 +575,41 @@ foreach $GENE (@all_genes) {
             $n_ntSingHap = $ntSingHap = scalar(keys %hash); 
             #print "ntSingHap is $ntSingHap\n";  
             #print "n_ntSingHap is $n_ntSingHap\n";
-            
+           
+            ##########################
+            # get rm uniqallele data #
+            ##########################
+            $temp = ();
+            @temp = ();
+            my @uniq = ();
+#            $nSingSNPs_R    = 0;
+            $n_ntSingHap_R  = 0;
+            $n_ntMajorHap_R = 0;
+            open F, '<', "$PARENTPATH/$GENE/$DATA_ID-removed.macse-nt-uniq.tsv"; # or die "Could not open uniqallele data\n";
+                while ($row = <F>) {
+                    chomp $row;
+                    push @uniq, $row;
+                }
+            close F;
+            # Getting haplotype numbers
+            #$ntHap [last position of column 0] #index 0
+            @temp = split /\t/, $uniq[$#uniq];
+            $ntHap_R = $temp[0];
+            %hash = ();
+            $n_ntMajorHap_R = $temp[$#temp] =~ tr{ }{ };
+
+            # Iterating through array to get other values
+            for $row (1..$#uniq) { #for each row index in uniq output
+                @temp = split /\t/, $uniq[$row]; # for each column in
+                $temp = $temp[$#temp] =~ tr{ }{ };
+                if ($temp == 1) {
+                    $hash{$temp[0]} = 1;
+                }
+            }
+            $n_ntSingHap_R = $ntSingHap_R = scalar(keys %hash);
+            #print "ntSingHap is $ntSingHap\n";
+            #print "n_ntSingHap is $n_ntSingHap\n";
+ 
             ######################
             # get PopGenome data #
             ######################
@@ -519,12 +703,31 @@ foreach $GENE (@all_genes) {
                 $aaDEL        . "\t" .
                 $aaFS         . "\t" .
                 $aaAM         . "\t" .
+                $ntTS_R       . "\t" .
+                $ntTV_R       . "\t" .
+                $ntIN_R       . "\t" .
+                $ntDEL_R      . "\t" .
+                $ntFS_R       . "\t" .
+                $ntAM_R       . "\t" .
+                $aaMS_R       . "\t" .
+                $aaNS_R       . "\t" .
+                $aaSYN_R      . "\t" .
+                $aaIN_R       . "\t" .
+                $aaDEL_R      . "\t" .
+                $aaFS_R       . "\t" .
+                $aaAM_R       . "\t" .
                 $ntHap        . "\t" .
                 $ntSingHap    . "\t" .
                 $nSegSite     . "\t" .
-                $nSingSNPs    . "\t" .
+#                $nSingSNPs    . "\t" .
                 $n_ntSingHap  . "\t" .
                 $n_ntMajorHap . "\t" .
+                $ntHap_R      . "\t" .
+                $ntSingHap_R  . "\t" .
+                $nSegSite_R   . "\t" .
+#                $nSingSNPs_R  . "\t" .
+                $n_ntSingHap_R. "\t" .
+                $n_ntMajorHap_R. "\t" .
                 $PG_TajimaD   . "\t" .
                 $PG_RozasR2   . "\t" .
                 $PG_FuliF     . "\t" .
@@ -535,17 +738,17 @@ foreach $GENE (@all_genes) {
                 $PG_Haplo     . "\t" .
                 $PG_SingHaps  . "\t" .
                 $PG_nSeqs     . "\t" .
-                $nRem        . "\t" .
-                $PG_TajimaD_R. "\t" .
-                $PG_RozasR2_R. "\t" .
-                $PG_FuliF_R  . "\t" .
-                $PG_FuliD_R  . "\t" .
-                $PG_FuFs_R   . "\t" .
-                $PG_pi_R     . "\t" .
-                $PG_SegSite_R. "\t" .
-                $PG_Haplo_R  . "\t" .
+                $nRem         . "\t" .
+                $PG_TajimaD_R . "\t" .
+                $PG_RozasR2_R . "\t" .
+                $PG_FuliF_R   . "\t" .
+                $PG_FuliD_R   . "\t" .
+                $PG_FuFs_R    . "\t" .
+                $PG_pi_R      . "\t" .
+                $PG_SegSite_R . "\t" .
+                $PG_Haplo_R   . "\t" .
                 $PG_SingHaps_R. "\t" .
-                $PG_nSeqs_R  . "\n";
+                $PG_nSeqs_R   . "\n";
 
     print O $output;
     print S $output;
